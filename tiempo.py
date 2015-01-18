9946cb6c338b4cfa24c04ee1047edde40faf8d8c
Sergio de Mingo <sdemingo@gmail.com>
2015-01-18 17:01:34 +0100

    modifico ecuaciones y hago gráfica

--- tiempo.py
+++ tiempo.py
@@ -10,13 +10,11 @@ Al-Naima and Bilal Abdul Majeed.
 Las ecuaciones de tiempo resumidas en el articulo estan citadas (por
 orden) en: :
 
-[1] P. J. Lunde, Solar Thermal Engineering—Space Heating and Hot Water
-    Systems, John Wiley & Sons, New York, NY, USA, 1980.
-[2] M. Iqbal, An Introduction to Solar Radiations, Academic Press, New
+[1] M. Iqbal, An Introduction to Solar Radiations, Academic Press, New
     York, NY, USA, 1986.
-[3] J. Kennewell and A. McDonald, “The equation of time,” Australian
+[2] J. Kennewell and A. McDonald, “The equation of time,” Australian
     Government, ISP-Radio and Space Services, http://www.ips.gov.au/.
-[4] G. M. Masters, Renewable and Efficient Electric Power Systems,
+[3] G. M. Masters, Renewable and Efficient Electric Power Systems,
     John Wiley & Sons, New York, NY, USA, 2004.  
 '''
 
@@ -29,14 +27,6 @@ hour_format='%H:%M'
 
 
 def ecuacionTiempo1(n):
-    if (n<100):
-        return -9*sin((n-1)/28.648) - 5
-    if (n>=100) and (n<=242):
-        return -5*sin((n-100)/22.632) - 1
-    if (n>242):
-        return -18.6*sin((n-242)/39.248) - 2
-
-def ecuacionTiempo2(n):
     w= (2*pi*(n-1))/365
     a=0.000075
     b=0.001868*cos(w)
@@ -46,12 +36,12 @@ def ecuacionTiempo2(n):
     et=229.18*(a+b-c-d-e)
     return et
 
-def ecuacionTiempo3(n):
+def ecuacionTiempo2(n):
     b= (360*(n-81))/365
     return 9.87 * sin(radians(2*b)) - 7.67*sin(radians(b + 78.7))
 
 
-def ecuacionTiempo4(n):
+def ecuacionTiempo3(n):
     b= (360*(n-81))/365
     return 9.87 * sin(radians(2*b)) - 7.53*cos(radians(b)) - 1.5*sin(radians(b))
 
--- tiempo_test.py
+++ tiempo_test.py
@@ -2,20 +2,40 @@
 # -*- coding: utf-8 -*- 
 
 
-
+import matplotlib.pyplot as plt
 from tiempo import *
-
+from numpy import *
 
 
 def main():
+    
+    res1=[]
+    res2=[]
+    res3=[]
+    for i in range(1,366):
+        res1.append(ecuacionTiempo1(i))
+        res2.append(ecuacionTiempo2(i))
+        res3.append(ecuacionTiempo3(i))
+        # et1=ecuacionTiempo1(i)
+        # et2=ecuacionTiempo2(i)
+        # et3=ecuacionTiempo3(i)
+        # print (i, et1,et2,et3)
 
-    for i in range(1,365):
-        et1=ecuacionTiempo1(i)
-        et2=ecuacionTiempo2(i)
-        et3=ecuacionTiempo3(i)
-        et4=ecuacionTiempo4(i)
-        print (i, et1,et2,et3,et4)
 
+    x = range(1,366)
+    y1 = res1
+    y2 = res2
+    y3 = res3
+    plt.plot(x,y1,color='r')
+    plt.plot(x,y2,color='b')
+    #plt.plot(x,y3,color='g')
+    plt.ylim(-17,+17)
+    plt.xlim(1,365)
+    plt.ylabel('minutos')
+    plt.xticks(linspace(1,365,5))
+    plt.show()
+
+  
 
 
 
