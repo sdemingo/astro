12da81a05a71ef727c4aa7428c25c480edc54e70
Sergio de Mingo <sdemingo@gmail.com>
2015-01-19 19:45:28 +0100

    solo una ecuación para gráfica

--- tiempo_test.py
+++ tiempo_test.py
@@ -9,25 +9,22 @@ from numpy import *
 
 def main():
     
-    res1=[]; res2=[]; res3=[]
+    res1=[]
     for i in range(1,366):
-        res1.append(ecuacionTiempo1(i))
-        res2.append(ecuacionTiempo2(i))
-        res3.append(ecuacionTiempo3(i))
-
+        res1.append(ecuacionTiempo3(i))
 
     x = range(1,366)
-    y1 = res1; y2 = res2; y3 = res3
+    y1 = res1
 
-    plt.plot(x,y1,color='r',label=u"ecuación 1")
-    plt.plot(x,y3,color='b',label=u"ecuación 3")
+    plt.plot(x,y1,color='b')
     plt.ylim(-17,+17)
     plt.xlim(1,365)
+    plt.title(u'Ecuación de tiempo')
     plt.ylabel('minutos')
+    plt.xlabel(u'días')
     plt.xticks(linspace(1,365,5))
     plt.grid()
     plt.axhline(0, color='black')
-    plt.legend(loc=4)
     plt.show()
 
   
