0ab5d7ef724a59e6653da29edefe3ec4de83fff3
Sergio de Mingo <sdemingo@gmail.com>
2015-09-22 17:21:29 +0200

    añado clase Catalog para mantener mi catalogo externo

--- skymap.py
+++ skymap.py
@@ -16,27 +16,53 @@ from sol import *
 
 
 
+class Catalog:
+
+    def __init__(self,filename):
+        self.stars={}
+        file = open(filename, "r")
+        for line in file.readlines():
+            key=line.split(",")[0]
+            self.stars[key]=line[:len(line)-1]
+            
+
+    
+
 
 class Constellation:
     
-    def __init__(self,obs):
+    def __init__(self,obs,catalog):
         self.stars={}
         self.points={}
+        self.catalog = catalog
         self.observer = obs
 
 
     def add_edge(self,fig,sname1, sname2, color='b'):
 
+        # if the stars name (sname1 or sname2)  are not defined in the
+        # default set of  pyephem it uses the external catalog
+
         if (sname1 not in self.stars):
-            s1=ephem.star(sname1)
-            s1.compute(self.observer)
+            try:
+                s1=ephem.star(sname1)
+                s1.compute(self.observer)
+            except:
+                s1=ephem.readdb(self.catalog.stars[sname1])
+                s1.compute(self.observer)
+ 
             p1=[math.degrees(s1.alt), math.degrees(s1.az)]
             self.stars[sname1]=s1
             self.points[sname1]=p1
 
         if (sname2 not in self.stars):
-            s2=ephem.star(sname2)
-            s2.compute(self.observer)
+            try:
+                s2=ephem.star(sname2)
+                s2.compute(self.observer)
+            except:
+                s2=ephem.readdb(self.catalog.stars[sname2])
+                s2.compute(self.observer)
+
             p2=[math.degrees(s2.alt), math.degrees(s2.az)]
             self.stars[sname2]=s2
             self.points[sname2]=p2
